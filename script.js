a2d25d5cf76862fc8f3b3a6e04bf1e5566565e96
Sekouba CAMARA <sekoubacamara100@gmail.com>
2026-02-14 12:25:23 -0500

    add some code in index, script, and styles

--- script.js
+++ script.js
@@ -4,18 +4,22 @@ const contentInput = document.getElementById("content")
 const titleSpan = document.getElementById("titleSpan")
 const contentSpan = document.getElementById("contentSpan")
 const list = document.getElementById("list")
-const cart = []
 let removeButton;
 
+// even listeners
+personalBlogForm.addEventListener("submit", formSubnit)
 
-personalBlogForm.addEventListener("submit", (event) => {
+// state
+let key = "STORAGE_KEY" 
+let cart = saveContent() //restore saved tasks from LocalStorage when the page reloads.
+
+
+function formSubnit(event) {
     event.preventDefault()
     const post = {
         title: titleInput.value.trim(),
         content: contentInput.value.trim()
     }
-
-
 
     if (title === "" | content === "") {
         alert("Please fill out all the fields")
@@ -23,15 +27,44 @@ personalBlogForm.addEventListener("submit", (event) => {
     }
 
     if (!title.validity.valid) {
-        title.setCustomvalidity("Please enter the title");
+        title.setCustomValidity("Please enter the title");
         titleSpan.textContent = title.validationMessage
         title.focus()
         return;
     }
     if (!content.validity.valid) {
-        content.setCustomvalidity("Please enter a content")
+        content.setCustomValidity("Please enter a content")
         contentSpan.textContent = content.validationMessage
         content.focus()
         return;
     }
-})
+
+    alert("form submitted")
+    clearInputs()
+}
+
+
+
+
+
+
+
+function clearInputs(){
+    titleInput.value = ""
+    contentInput.value = ""
+}
+
+// local storage
+function saveContent(){
+    // localStorage.setItem(key, JSON.stringify(cart)) // save under varible key
+}
+
+// load data 
+function loadContent(){
+    const data = localStorage.getItem(key)
+    if (!data) return [];
+
+    // prevent crashes and if data is corrupted
+    try{return JSON.parse(data);} 
+    catch{return [];} 
+}
